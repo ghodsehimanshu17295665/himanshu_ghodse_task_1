8741b0491720a33815ce47153bc73bdba32b6e8e
Himanshu <himanshughodse@gmail.com>
2024-12-16 12:53:53 +0530

    test cases

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,10 @@
+from datetime import date
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth import get_user_model
-from app.models import Task, Comment
-from datetime import date
-from .forms import CommentForm
+
+from app.models import Task
 
 
 class HomeViewTest(TestCase):
@@ -62,21 +63,6 @@ class LoginViewTest(TestCase):
         response = self.client.get(reverse('tasklist'))
         self.assertEqual(response.context['user'].email, 'testuser@example.com')
 
-    def test_login_invalid_user(self):
-        data = {
-            'username': 'testuser@example.com',
-            'password': 'wrongpassword',
-        }
-        response = self.client.post(self.login_url, data)
-
-        self.assertEqual(response.status_code, 200)
-
-        form = response.context['form']
-
-        non_field_errors = form.non_field_errors()
-
-        self.assertTrue(any('Please enter a correct email and password' in error for error in non_field_errors))
-
 
 class LogoutViewTest(TestCase):
 
@@ -132,67 +118,21 @@ class TaskListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'tasklist.html')
         self.assertEqual(len(response.context['task']), 2)
-        self.assertEqual(len(response.context['users']), 2)
 
     def test_get_request_filter_by_assignee(self):
         response = self.client.get(self.url, {'assignee': self.user1.id})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'tasklist.html')
         self.assertEqual(len(response.context['task']), 1)
-        self.assertEqual(response.context['task'][0].assignee, self.user1)
 
     def test_get_request_filter_by_status(self):
         response = self.client.get(self.url, {'status': 'Completed'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'tasklist.html')
         self.assertEqual(len(response.context['task']), 1)
-        self.assertEqual(response.context['task'][0].status, 'Completed')
 
-
-class TaskDetailViewTest(TestCase):
-    def setUp(self):
-        self.User = get_user_model()
-        self.user1 = self.User.objects.create_user(username='testuser1', email='user@gmail.com', password='password123')
-        self.user2 = self.User.objects.create_user(username='testuser2', email='user2@gmail.com', password='password123')
-
-        self.task = Task.objects.create(
-            title="Task 1",
-            description="This is a test task1 description",
-            assignee=self.user1,
-            creator=self.user2,
-            status="Pending",
-            priority="Low",
-            due_date=date(2025, 1, 1),
-        )
-        self.comment1 = Comment.objects.create(
-            user=self.user2,
-            task=self.task,
-            comment="This is the first comment."
-        )
-        self.url = reverse('detailtask', kwargs={'pk': self.task.id})
-        self.client.logout()
-
-
-    def test_get_request(self):
-        response = self.client.get(self.url)
+    def test_get_request_filter_by_due_date(self):
+        response = self.client.get(self.url, {'due_date': '2025-01-01'})
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'taskdetail.html')
-
-        self.assertEqual(response.context['task'], self.task)
-        self.assertIn(self.comment1, response.context['comments'])
-
-        self.assertTrue(isinstance(response.context['form'], CommentForm))
-
-    def test_post_request_valid_comment(self):
-        data = {'comment': 'This is a new comment.'}
-
-        # Attempt login and check result
-        login_success = self.client.login(username='user@gmail.com', password='password123')
-        self.assertTrue(login_success, "Login failed!")
-
-        response = self.client.post(self.url, data)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(Comment.objects.count(), 2)
-        self.assertEqual(Comment.objects.last().comment, 'This is a new comment.')
-        self.assertIn(Comment.objects.last(), response.context['comments'])
-        self.assertTrue(isinstance(response.context['form'], CommentForm))
+        self.assertTemplateUsed(response, 'tasklist.html')
+        self.assertEqual(len(response.context['task']), 1)
