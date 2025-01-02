af34ef5a3b6151b08364353ecf66bf811e41682d
Himanshu <himanshughodse@gmail.com>
2025-01-02 14:15:22 +0530

    setting file

--- task/settings.py
+++ task/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -86,14 +86,36 @@ WSGI_APPLICATION = "task.wsgi.application"
 #     }
 # }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "taskmanagement",
+#         "USER": "taskuser",
+#         "PASSWORD": "securepassword123",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME', 'taskmanagement'),
+#         'USER': os.getenv('DB_USER', 'taskuser'),
+#         'PASSWORD': os.getenv('DB_PASSWORD', 'securepassword123'),
+#         'HOST': os.getenv('DB_HOST', 'localhost'),
+#         'PORT': os.getenv('DB_PORT', '5432'),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "taskmanagement",
-        "USER": "taskuser",
-        "PASSWORD": "securepassword123",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'taskmanagement'),
+        'USER': os.getenv('POSTGRES_USER', 'taskuser'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'securepassword123'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432')
     }
 }
 
