14edf3d89fa7b9e0ed6d7d019cf4afa13074bc0c
Himanshu Ghodse <himanshughodse@gmail.com>
2025-08-05 18:05:20 +0530

    Add static file support using WhiteNoise

--- task/settings.py
+++ task/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -161,6 +162,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Media files (Uploaded by users)
 MEDIA_URL = "/media/"
