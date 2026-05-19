d8d0e051b2bead5d40a12b04a56e80a74709a1a9
Himanshu Ghodse <himanshughodse@gmail.com>
2026-05-19 18:00:08 +0530

    fix: add PostgreSQL wait logic and install netcat for container startup

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,14 @@
-# Use the official Python image as the base image
 FROM python:3.9
 
-# Set the working directory inside the container
 WORKDIR /app
 
-# Copy the Django project files into the container
 COPY . /app/
 
-# Install dependencies
+#  Install netcat + dependencies
+RUN apt-get update && apt-get install -y netcat
+
 RUN pip install -r requirements.txt
 
 RUN chmod +x /app/server-entrypoint.sh
 
-# Expose the port that Django runs on
 EXPOSE 8000
--- server-entrypoint.sh
+++ server-entrypoint.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
-/wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Database is up!"
+#!/bin/sh
 
-# Apply database migrations
+set -e
+
+echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "Postgres not ready... retrying"
+  sleep 1
+done
+
+echo "PostgreSQL started"
+
 python manage.py migrate
 
-# Start the Django development server
+echo "Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
