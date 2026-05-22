0a98efcb7ed5240dc60e4f7de048d53e8ec05904
Himanshu Ghodse <himanshughodse@gmail.com>
2026-05-22 14:04:17 +0530

    Update server-entrypoint file

--- server-entrypoint.sh
+++ server-entrypoint.sh
@@ -2,16 +2,27 @@
 
 set -e
 
-echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
-
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "Postgres not ready... retrying"
-  sleep 1
-done
-
-echo "PostgreSQL started"
+echo "Starting Django without blocking DB wait..."
 
 python manage.py migrate
 
-echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000
+echo "Starting server..."
+exec python manage.py runserver 0.0.0.0:8000
+
+# #!/bin/sh
+
+# set -e
+
+# echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
+
+# while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+#   echo "Postgres not ready... retrying"
+#   sleep 1
+# done
+
+# echo "PostgreSQL started"
+
+# python manage.py migrate
+
+# echo "Starting Django server..."
+# python manage.py runserver 0.0.0.0:8000
