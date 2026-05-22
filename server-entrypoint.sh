f53a8961459c50de2d1695eed48911a8cbc5bdc5
Himanshu Ghodse <himanshughodse@gmail.com>
2026-05-22 17:44:38 +0530

    fix: fix ALB 502 issue by correcting health check to /health/ and stabilizing EC2 deployment

--- server-entrypoint.sh
+++ server-entrypoint.sh
@@ -2,27 +2,22 @@
 
 set -e
 
-echo "Starting Django without blocking DB wait..."
+echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
 
-python manage.py migrate
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "Postgres not ready... retrying"
+  sleep 2
+done
+
+echo "PostgreSQL is up!"
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 echo "Starting server..."
-exec python manage.py runserver 0.0.0.0:8000
 
-# #!/bin/sh
-
-# set -e
-
-# echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
-
-# while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-#   echo "Postgres not ready... retrying"
-#   sleep 1
-# done
-
-# echo "PostgreSQL started"
-
-# python manage.py migrate
-
-# echo "Starting Django server..."
-# python manage.py runserver 0.0.0.0:8000
+# PRODUCTION server (better than runserver)
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
